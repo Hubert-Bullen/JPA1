613c74957f5ed3188139042813754cdff66ee471
Hubert Bullen <Hubert_99_bullen@hotmail.com>
2015-05-26 11:49:40 +0200

    Exercise 1 over.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,9 @@
 insert into book(title, author, isbn) values('Nineteen Eighty Four', 'George Orwell', '0000000001');
 
 insert into book(title, author, isbn) values('Alice''s Adventures In Wonderland', 'Lewis Carroll', '0000000002');
+
+insert into book(title, author, isbn) values('Shogun', 'James Clavell', '0000000003');
+
+insert into book(title, author, isbn) values('Coraline', 'Neil Gaiman', '0000000004');
+
+insert into book(title, author, isbn) values('Norwegian Wood', 'Haruki Murakami', '0000000005');
--- src/test/java/EntityManagerFactoryTest.java
+++ src/test/java/EntityManagerFactoryTest.java
@@ -1,9 +1,36 @@
 import junit.framework.TestCase;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
 
 /**
  * Created by Hyuberuto on 26/05/15.
  */
 public class EntityManagerFactoryTest extends TestCase {
+
+
+    public void testOurLogic() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("RealDolmenPersistenceUnit");
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        BookRepositoryDao bookRepository = new BookRepositoryBean(em);
+        List<Book> books = bookRepository.findAllBooks();
+        assertEquals(5, books.size());
+
+        em.getTransaction().commit();
+        em.close();
+        entityManagerFactory.close();
+    }
+
+
+
+
+
+
+
+
 
 
 }
